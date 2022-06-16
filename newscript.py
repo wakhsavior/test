649a8ac9f278ebc8b262e07c4c5161c576d4edf2
wakhsavior <wakhsavior@gmail.com>
2022-06-16 19:24:10 +0300

    add function

--- newscript.py
+++ newscript.py
@@ -1,5 +1,16 @@
 import sys
 
+def check_add(used, cab):    # used - заполняемая бухта    cab - текущий кабель
+    r = False
+    p_used = used + cab      #  Добавляем кабель в бухту для проверки
+    p_ost = size - p_used     #   Проверяем остаток после добавления кабеля
+    if p_used <= size:          # Проверяем, умещается ли кусок в бухту
+        if p_ost <= dopusk:     # Проверяем, находится ли остаток в допуске     
+            r = True
+        if p_ost >= cab:        # Если остаток  кабеля больше длины самого кабеля
+            r = True
+    return r
+
 
 def main(argv):
     num = 100
